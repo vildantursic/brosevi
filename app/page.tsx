d37f19cd8cd191a2a80a119bd2f5b5b4086f2da7
Vildan Tursic <vildantursic@hotmail.com>
2022-12-19 22:35:17 +0100

    sold out update

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,66 @@
 import Image from "next/image";
 
 const images = [
-  "6641",
-  "6646",
-  "6650",
-  "6652",
-  "6656",
-  "6658",
-  "6662",
-  "6666",
-  "6669",
-  "6672",
-  "6677",
-  "6680",
-  "6685",
-  "6690",
-  "6696",
+  {
+    id: 6641,
+    sold: false,
+  },
+  {
+    id: 6646,
+    sold: true,
+  },
+  {
+    id: 6650,
+    sold: false,
+  },
+  {
+    id: 6652,
+    sold: false,
+  },
+  {
+    id: 6656,
+    sold: false,
+  },
+  {
+    id: 6658,
+    sold: false,
+  },
+  {
+    id: 6662,
+    sold: false,
+  },
+  {
+    id: 6666,
+    sold: false,
+  },
+  {
+    id: 6669,
+    sold: false,
+  },
+  {
+    id: 6672,
+    sold: false,
+  },
+  {
+    id: 6677,
+    sold: false,
+  },
+  {
+    id: 6680,
+    sold: false,
+  },
+  {
+    id: 6685,
+    sold: false,
+  },
+  {
+    id: 6690,
+    sold: false,
+  },
+  {
+    id: 6696,
+    sold: false,
+  },
 ];
 
 export default function Home() {
@@ -24,12 +69,17 @@ export default function Home() {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 grid-flow-row gap-10">
         {images.map((image, index) => (
           <div
-            key={`image-${image}`}
+            key={`image-${image.id}`}
             className="flex items-center justify-center relative overflow-hidden border-2 border-solid border-black rounded-lg"
           >
+            {image.sold && (
+              <div className="absolute top-0 w-full h-[60px] bg-red-600/70 text-white flex items-center justify-center font-extrabold">
+                SOLD OUT
+              </div>
+            )}
             <Image
               alt="brosevi"
-              src={`/images/IMG_${image}.jpg`}
+              src={`/images/IMG_${image.id}.jpg`}
               width={700}
               height={700}
             ></Image>
